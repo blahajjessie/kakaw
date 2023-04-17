6649e0d1ab7362c5f4f3f99de0b079678f9b8242
Ben Grant <benjamin.j.grant@gmail.com>
2023-04-16 18:03:11 -0700

    Initial backend with HTTP server

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+	res.send('Hello, world!');
+});
+
+app.listen(8080).on('listening', () => {
+	console.log('listening on localhost:8080');
+});
