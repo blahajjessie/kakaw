6e4b69f9335b672053eef7383db97d8ec3dd5ecd
celdond <celdond@gmail.com>
2023-04-19 02:34:44 -0700

    frontend testing set-up fixed with Next.js

--- frontend/__tests__/app.test.tsx
+++ frontend/__tests__/app.test.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import {render} from '@testing-library/react';
-import "@testing-library/jest-dom";
-import Home from '../pages/index'; 
+import Home from '@/pages/index';
 
 describe('Home', () => {
     it('should render', () => {
