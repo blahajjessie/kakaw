91429273fe2a2000e811ab794f2fe3ff9aef75a1
Ben Grant <benjamin.j.grant@gmail.com>
2023-04-22 15:30:58 -0700

    Add helper for sending API requests

---
+++ frontend/lib/api.ts
@@ -0,0 +1,18 @@
+export const API_BASE_URL = 'http://localhost:8080';
+export const WEBSOCKET_BASE_URL = 'ws://localhost:8080';
+
+export function apiCall(
+	method: 'GET' | 'POST',
+	url: string,
+	body?: any
+): Promise<Response> {
+	const options: RequestInit = { method };
+	if (body) {
+		options.body = body;
+		options.headers = {
+			'content-type': 'application/json',
+		};
+	}
+
+	return fetch(API_BASE_URL + url, options);
+}
