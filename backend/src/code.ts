2aa62aa4c38ffcc5767629f28ad0ffd98ae98197
rlchoy <rlchoy@ucsc.edu>
2023-04-21 20:55:37 -0700

    task 7: camel case + removed alpha

--- backend/src/code.ts
+++ backend/src/code.ts
@@ -1,25 +1,25 @@
 export function gen(len: number, used: Array<string>) {
-	let result = '';
-	const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	const char_len = char.length;
+    let result = '';
+    const char = '0123456789';
+    const charLen = char.length;
 
-	do {
-		// generate code
-		for (let i = 0; i < len; i++) {
-			result += char.charAt(Math.floor(Math.random() * char_len));
-		}
+    do {
+        // generate code
+        for (let i = 0; i < len; i++) {
+			result += char.charAt(Math.floor(Math.random() * charLen));
+        }
 
-		// checks for duplicates
-		if (used.includes(result)) {
-			result = '';
-		}
-	} while (result == '');
+        // checks for duplicates
+        if (used.includes(result)) {
+            result = '';
+        }
+    } while (result == '');
 
-	used.push(result);
-	return result;
+    used.push(result);
+    return result;
 }
 
 export function fin(code: string, used: Array<string>) {
-	const index = used.indexOf(code);
-	used.splice(index, 1);
+    const index = used.indexOf(code);
+    used.splice(index, 1);
 }
