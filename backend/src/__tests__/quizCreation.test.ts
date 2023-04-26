671c06abecf0d3bbdfde23692ca2635c7fa12140
celdond <celdond@gmail.com>
2023-04-25 18:35:33 -0700

    tests now focus on quiz uploading adjusts tsconfig.json to allow loading json for testing purposes

---
+++ backend/src/__tests__/quizCreation.test.ts
@@ -0,0 +1,29 @@
+import httpServer from '../server';
+import supertest from 'supertest';
+import correct from './testTools/correct.json';
+
+let request: supertest.SuperTest<supertest.Test>;
+
+beforeAll(() => {
+	request = supertest(httpServer);
+});
+
+afterAll((done) => {
+	httpServer.close(done);
+});
+
+test('GET Invalid URL', async () => {
+	await request.get('/so-not-a-real-end-point-ba-bip-de-doo-da/').expect(404);
+});
+
+const bad_format = {
+	ooga: 'booga',
+};
+
+test('Wrong Quiz Format', async () => {
+	await request.post('/games').send(bad_format).expect(400);
+});
+
+test('Correct Quiz Format', async () => {
+	await request.post('/games').send(correct).expect(201);
+});
