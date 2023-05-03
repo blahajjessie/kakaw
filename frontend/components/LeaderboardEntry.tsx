6f580b4dc9c2460338d391c0ebfd3ff5fce6726d
Jasmine Tai <jasminecktai@gmail.com>
2023-05-02 22:13:11 -0700

    Basic desktop leaderboard page

---
+++ frontend/components/LeaderboardEntry.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+
+import lb_up from 'public/lb_up.svg';
+import lb_equal from 'public/lb_equal.svg';
+
+interface QuestionAnswerProps {
+	name: string;
+	score: number;
+	// positionChange: positive for up, 0 for no change, negative for down
+	positionChange: number;
+	// isSelf: true if the entry represents the current player
+	isSelf: boolean;
+}
+
+export default function LeaderboardEntry({
+	name,
+	score,
+	isSelf,
+	positionChange,
+}: QuestionAnswerProps) {
+	// show up, down, or equal icon based on if/how player's position has changed
+	const positionIcon =
+		positionChange > 0 ? (
+			<Image alt="position change icon" src={lb_up} width={20} />
+		) : positionChange < 0 ? (
+			<Image
+				alt="position change icon"
+				src={lb_up}
+				width={20}
+				className="rotate-180"
+			/>
+		) : (
+			<Image alt="position change icon" src={lb_equal} width={20} />
+		);
+
+	if (isSelf) {
+		return (
+			<div className="bg-gray-100 bg-opacity-50 outline outline-3 outline-white flex flex-row items-center pl-12 pr-4 py-2 my-5 text-lg text-white rounded-lg shadow-heavy hover:brightness-110">
+				<span>You</span>
+				<span className="ml-auto mr-4">{score}</span>
+				{positionIcon}
+			</div>
+		);
+	} else {
+		return (
+			<div className="bg-gray-100 bg-opacity-50 outline outline-1 outline-gray-200 flex flex-row items-center pl-8 pr-4 py-2 my-5 text-lg text-white rounded-lg shadow-heavy hover:brightness-110">
+				<span>{name}</span>
+				<span className="ml-auto mr-4">{score}</span>
+				{positionIcon}
+			</div>
+		);
+	}
+}
--- frontend/pages/[gameId]/leaderboard.tsx
+++ frontend/pages/[gameId]/leaderboard.tsx
@@ -1,14 +1,82 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
+import MatchMediaWrapper from '@/components/MatchMediaWrapper';
+import LeaderboardEntry from '@/components/LeaderboardEntry';
+
 import logo2 from 'public/logo2.png';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const leaderboardEntries = [
+	{
+		name: 'Player 1',
+		score: 2000,
+		positionChange: 1,
+		isSelf: false,
+	},
+	{
+		name: 'Player 2',
+		score: 1500,
+		positionChange: -1,
+		isSelf: true,
+	},
+	{
+		name: 'Player 3',
+		score: 1100,
+		positionChange: 0,
+		isSelf: false,
+	},
+	{
+		name: 'Player 4',
+		score: 800,
+		positionChange: 2,
+		isSelf: false,
+	},
+	{
+		name: 'Player 5',
+		score: 700,
+		positionChange: -1,
+		isSelf: false,
+	},
+	{
+		name: 'Player 6',
+		score: 700,
+		positionChange: -1,
+		isSelf: false,
+	},
+];
+
 export default function Home() {
-	return (
+	leaderboardEntries.sort((a, b) => b.score - a.score);
+
+	const entriesList = leaderboardEntries.map((entry, i) => (
+		<LeaderboardEntry
+			key={`${entry.name}_${i}`}
+			name={entry.name}
+			score={entry.score}
+			positionChange={entry.positionChange}
+			isSelf={entry.isSelf}
+		/>
+	));
+
+	const mobileContent = (
 		<main className="w-full h-screen bg-purple-100 flex flex-col items-center justify-center font-extrabold">
-			<button className="absolute top-4 right-4 bg-purple-50 self-end px-8 py-2 rounded-lg text-xl text-white shadow-heavy">
+			<div className="w-4/5 h-full flex flex-col items-center justify-center">
+				<Image alt="logo2" src={logo2} width={150} className="-mb-6" />
+				<div className="w-min bg-gray-100 border border-black px-4 py-1 text-3xl z-20">
+					Leaderboard
+				</div>
+				<div className="w-full h-2/3 bg-purple-500 p-14 -mt-6 rounded-xl overflow-y-auto z-10">
+					{entriesList}
+				</div>
+			</div>
+		</main>
+	);
+
+	const desktopContent = (
+		<main className="w-full h-screen bg-purple-100 flex flex-col items-center justify-center font-extrabold">
+			<button className="absolute top-4 right-4 bg-purple-50 self-end px-8 py-2 rounded-lg text-xl text-white shadow-heavy hover:brightness-110">
 				Continue
 			</button>
 			<div className="w-4/5 h-full flex flex-col items-center justify-center">
@@ -16,17 +84,17 @@ export default function Home() {
 				<div className="w-min bg-gray-100 border border-black px-4 py-1 text-3xl z-20">
 					Leaderboard
 				</div>
-				<div className="w-full h-2/3 bg-purple-500 pl-14 pr-24 py-14 -mt-6 rounded-xl z-10">
-					<div className="bg-gray-100 bg-opacity-50 flex flex-row items-center pl-12 py-2 my-4 text-lg text-white rounded-lg">
-						<span>You</span>
-						<span className="ml-auto">2000</span>
-					</div>
-					<div className="bg-gray-100 bg-opacity-50 flex flex-row items-center pl-8 py-2 my-4 text-lg text-white rounded-lg">
-						<span>Player 1</span>
-						<span className="ml-auto">1500</span>
-					</div>
+				<div className="w-full h-2/3 bg-purple-500 p-14 -mt-6 rounded-xl overflow-y-auto z-10">
+					{entriesList}
 				</div>
 			</div>
 		</main>
+	);
+
+	return (
+		<MatchMediaWrapper
+			mobileContent={mobileContent}
+			desktopContent={desktopContent}
+		/>
 	);
 }
