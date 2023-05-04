1e39259c094a4ae7f95fd7208b08ecce19cbf4ea
celdond <celdond@gmail.com>
2023-05-03 19:02:04 -0700

    css and array specifics

--- frontend/components/WaitingPages/HostWaiting.tsx
+++ frontend/components/WaitingPages/HostWaiting.tsx
@@ -3,15 +3,22 @@ import { Inter } from 'next/font/google';
 import { apiCall } from '@/lib/api';
 import { ChangeEvent, useState, useContext } from 'react';
 
-import logo2 from '../public/logo2.png';
-import { playerListContextType, playerListContext } from '@/components/Context/ShareContext';
+import logo2 from '@/public/logo2.png';
+import {
+	playerListContextType,
+	playerListContext,
+} from '@/components/Context/ShareContext';
 
 const inter = Inter({
 	subsets: ['latin'],
 	variable: '--font-inter',
 });
 
-export default function hostWaiting(hostId: number) {
+interface hostProps {
+	hostId: string;
+}
+
+export default function HostWaiting({ hostId }: hostProps) {
 	const [timeLimit, setTimeLimit] = useState<string | null>(null);
 	const [maxPlayers, setMaxPlayers] = useState<string | null>(null);
 	const playerList = useContext(playerListContext) as playerListContextType;
@@ -28,20 +35,25 @@ export default function hostWaiting(hostId: number) {
 		setMaxPlayers(newMax);
 	}
 
+	// Sends the Server a call to start the game
+	// NOTE: The server does send json to respond
 	async function startQuiz() {
-		const { ok, err } = await apiCall(
-			'POST',
-			`/games/${hostId}/questions/0/start`
-		)
+		apiCall('POST', `/games/${hostId}/questions/0/start`)
 			.then((res) => {
 				if (!res.ok) {
 					throw res;
 				}
-				return res.json();
+				return;
 			})
 			.catch((err) => {
 				alert(`Error starting game (${err}), please try again`);
 			});
+	}
+
+	const displayPlayers: string[][] = [];
+	for (let i = 0; i < playerList.length; i += 4) {
+		const row = playerList.slice(i, i + 4);
+		displayPlayers.push(row);
 	}
 
 	return (
@@ -49,38 +61,40 @@ export default function hostWaiting(hostId: number) {
 			className={`${inter.variable} w-full font-sans bg-purple-100 flex flex-col p-8`}
 		>
 			<div className="flex flex-row items-center justify-center w-full">
-				<div className="bg-gray-100 w-full flex flex-row items-center justify-between font-extrabold shadow-heavy rounded-xl p-8">
+				<div className="bg-gray-100 w-full flex flex-row items-center justify-between font-extrabold shadow-heavy rounded-xl p-6">
 					<div>
 						<div className="text-4xl">Join with the code:</div>
-						<div className="text-8xl">XXXXX</div>
+						<div className="text-8xl">{hostId}</div>
 					</div>
 					<div className="flex flex-col p-4">
-						<div className="text-4xl">Options:</div>
+						<div className="text-4xl p-2">Options:</div>
 						<div className="flex flex-row">
-							<div className="flex flex-col text-2xl">
-								<div>Time Limit:</div>
-								<div>Max Players:</div>
-							</div>
-							<div className="flex flex-col p-4">
-								<input
-									className="flex flex-col"
-									id="time"
-									type="text"
-									maxLength={3}
-									onChange={handleTimeChange}
-								/>
-								<input
-									className="flex flex-col"
-									id="maxPlayers"
-									type="text"
-									maxLength={4}
-									onChange={handlePlayerChange}
-								/>
+							<div className="flex flex-row p-2">
+								<div className="flex flex-col text-2xl">
+									<div className="mb-2">Time Limit:</div>
+									<div className="mb-2">Max Players:</div>
+								</div>
+								<div className="flex flex-col text-2xl px-3">
+									<input
+										className="w-28 mb-2"
+										id="time"
+										type="text"
+										maxLength={3}
+										onChange={handleTimeChange}
+									/>
+									<input
+										className="w-28 mb-2"
+										id="maxPlayers"
+										type="text"
+										maxLength={4}
+										onChange={handlePlayerChange}
+									/>
+								</div>
 							</div>
 						</div>
 					</div>
 					<button
-						className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl p-8 mx-2 text-white text-center text-6xl shadow-md"
+						className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl shadow-heavy w-48 h-20"
 						type="button"
 						onClick={() => {
 							startQuiz;
@@ -91,6 +105,7 @@ export default function hostWaiting(hostId: number) {
 				</div>
 				<Image
 					alt="Kakaw logo"
+					className="p-8"
 					src={logo2}
 					width={189}
 					style={{
@@ -99,9 +114,17 @@ export default function hostWaiting(hostId: number) {
 					}}
 				/>
 			</div>
-			<div className="flex flex-col items-start justify-start">
-				<div className="text-4xl font-extrabold">Participants (x)</div>
-				<div>{playerList}</div>
+			<div className="flex flex-col items-start justify-start p-10">
+				<div className="text-4xl font-extrabold py-4">
+					Participants ({playerList.length})
+				</div>
+				<table className="w-full text-2xl font-extrabold">{displayPlayers.map((subArray, index) => (
+					<tr key={index}>
+						{subArray.map((player, i) => (
+							<td className="py-2" key={index + i}>{player}</td>
+						))}
+					</tr>
+				))}</table>
 			</div>
 		</main>
 	);
