2f9315b770c947ce06a8264680b3c1f5a70564a6
celdond <celdond@gmail.com>
2023-05-03 20:44:40 -0700

    css changes

--- frontend/components/WaitingPages/HostWaiting.tsx
+++ frontend/components/WaitingPages/HostWaiting.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import { apiCall } from '@/lib/api';
 import { ChangeEvent, useState, useContext } from 'react';
+import MatchMediaWrapper from '@/components/MatchMediaWrapper';
 
 import logo2 from '@/public/logo2.png';
 import {
@@ -10,8 +11,7 @@ import {
 } from '@/components/Context/ShareContext';
 
 const inter = Inter({
-	subsets: ['latin'],
-	variable: '--font-inter',
+	subsets: ['latin']
 });
 
 interface hostProps {
@@ -56,75 +56,109 @@ export default function HostWaiting({ hostId }: hostProps) {
 		displayPlayers.push(row);
 	}
 
+	const headerContent = (
+		<div className="flex flex-col p-4">
+			<div className="text-4xl p-2">Options:</div>
+			<div className="flex flex-row">
+				<div className="flex flex-row p-2">
+					<div className="flex flex-col text-2xl">
+						<div className="mb-2">Time Limit:</div>
+						<div className="mb-2">Max Players:</div>
+					</div>
+					<div className="flex flex-col text-2xl px-3">
+						<input
+							className="w-28 mb-2"
+							id="time"
+							type="text"
+							maxLength={3}
+							onChange={handleTimeChange}
+						/>
+						<input
+							className="w-28 mb-2"
+							id="maxPlayers"
+							type="text"
+							maxLength={4}
+							onChange={handlePlayerChange}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+
+	const desktopContent = (
+		<div className="flex flex-row items-center justify-start w-full">
+			<div className="bg-gray-100 flex flex-row items-center justify-between font-extrabold shadow-heavy rounded-xl p-6">
+				<div>
+					<div className="text-4xl">Join with the code:</div>
+					<div className="text-8xl">{hostId}</div>
+				</div>
+				{headerContent}
+				<button
+					className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl shadow-heavy w-48 h-20"
+					type="button"
+					onClick={() => {
+						startQuiz;
+					}}
+				>
+					Start
+				</button>
+			</div>
+			<Image
+				alt="Kakaw logo"
+				className="mx-4"
+				src={logo2}
+				width={189}
+				style={{
+					maxWidth: '100%',
+					height: 'auto',
+				}}
+			/>
+		</div>
+	);
+
+	const mobileContent = (
+		<div className="bg-gray-100 w-full flex flex-col items-center justify-center font-extrabold shadow-heavy rounded-xl p-4">
+			<div className="flex flex-col items-center justify-center">
+				<div className="text-4xl">Join with the code:</div>
+				<div className="text-8xl">{hostId}</div>
+			</div>
+			{headerContent}
+			<button
+				className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl shadow-heavy w-48 h-20"
+				type="button"
+				onClick={() => {
+					startQuiz;
+				}}
+			>
+				Start
+			</button>
+		</div>
+	);
+
 	return (
 		<main
-			className={`${inter.variable} w-full font-sans bg-purple-100 flex flex-col p-8`}
+			className={`${inter.className} w-full font-sans bg-purple-100 flex flex-col p-6`}
 		>
-			<div className="flex flex-row items-center justify-center w-full">
-				<div className="bg-gray-100 w-full flex flex-row items-center justify-between font-extrabold shadow-heavy rounded-xl p-6">
-					<div>
-						<div className="text-4xl">Join with the code:</div>
-						<div className="text-8xl">{hostId}</div>
-					</div>
-					<div className="flex flex-col p-4">
-						<div className="text-4xl p-2">Options:</div>
-						<div className="flex flex-row">
-							<div className="flex flex-row p-2">
-								<div className="flex flex-col text-2xl">
-									<div className="mb-2">Time Limit:</div>
-									<div className="mb-2">Max Players:</div>
-								</div>
-								<div className="flex flex-col text-2xl px-3">
-									<input
-										className="w-28 mb-2"
-										id="time"
-										type="text"
-										maxLength={3}
-										onChange={handleTimeChange}
-									/>
-									<input
-										className="w-28 mb-2"
-										id="maxPlayers"
-										type="text"
-										maxLength={4}
-										onChange={handlePlayerChange}
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-					<button
-						className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl shadow-heavy w-48 h-20"
-						type="button"
-						onClick={() => {
-							startQuiz;
-						}}
-					>
-						Start
-					</button>
-				</div>
-				<Image
-					alt="Kakaw logo"
-					className="p-8"
-					src={logo2}
-					width={189}
-					style={{
-						maxWidth: '100%',
-						height: 'auto',
-					}}
-				/>
-			</div>
+			<MatchMediaWrapper
+				mobileContent={mobileContent}
+				desktopContent={desktopContent}
+			/>
 			<div className="flex flex-col items-start justify-start p-10">
 				<div className="text-4xl font-extrabold py-4">
 					Participants ({playerList.length})
 				</div>
-				<table className="w-full text-2xl font-extrabold">{displayPlayers.map((subArray, index) => (
-					<tr key={index}>
-						{subArray.map((player, i) => (
-							<td className="py-2" key={index + i}>{player}</td>
-						))}
-					</tr>
-				))}</table>
+				<table className="w-full text-2xl font-extrabold">
+					{displayPlayers.map((subArray, index) => (
+						<tr key={index}>
+							{subArray.map((player, i) => (
+								<td className="py-4" key={index + i}>
+									{player}
+								</td>
+							))}
+						</tr>
+					))}
+				</table>
 			</div>
 		</main>
 	);
