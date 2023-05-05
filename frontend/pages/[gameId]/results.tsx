bb44045562dc7c14ee4819126cca64eeedcc1462
Jasmine Tai <jasminecktai@gmail.com>
2023-05-05 00:54:06 -0700

    Added global font and desktop results

--- frontend/pages/[gameId]/results.tsx
+++ frontend/pages/[gameId]/results.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
 
-import MatchMediaWrapper from '@/components/MatchMediaWrapper';
-import results_background from '@/public/results_background.png';
-
-const inter = Inter({ subsets: ['latin'] });
+import medal1 from '@/public/medal1.png';
+import medal2 from '@/public/medal2.png';
+import medal3 from '@/public/medal3.png';
 
 const resultsEntries = [
 	{
@@ -30,22 +28,29 @@ const resultsEntries = [
 export default function ResultsPage() {
 	resultsEntries.sort((a, b) => b.score - a.score);
 
-	const mobileContent = (
-		<main className="w-full h-screen bg-purple-100 flex flex-col items-center justify-center font-extrabold">
-			<div className=""></div>
-		</main>
-	);
-
-	const desktopContent = (
-		<main className="w-full h-screen bg-purple-100 flex flex-col items-center justify-center font-extrabold">
-			<div className=""></div>
-		</main>
-	);
-
 	return (
-		<MatchMediaWrapper
-			mobileContent={mobileContent}
-			desktopContent={desktopContent}
-		/>
+		<main className="w-full h-screen bg-purple-100 bg-results bg-fixed bg-center bg-cover flex flex-row items-end justify-center font-extrabold text-3xl text-shadow shadow-gray-200">
+			<div className="w-1/4 h-full flex flex-col items-center justify-end">
+				<div className="text-white mb-0.5">{resultsEntries[1].name}</div>
+				<div className="text-white mb-1">{resultsEntries[1].score}</div>
+				<div className="w-full h-2/5 bg-gray-400 flex items-start justify-center p-8">
+					<Image alt="medal 2" src={medal2} width={110} />
+				</div>
+			</div>
+			<div className="w-1/4 h-full flex flex-col items-center justify-end">
+				<div className="text-white mb-0.5">{resultsEntries[0].name}</div>
+				<div className="text-white mb-1">{resultsEntries[0].score}</div>
+				<div className="w-full h-2/3 bg-gray-100 flex items-start justify-center p-8">
+					<Image alt="medal 1" src={medal1} width={120} />
+				</div>
+			</div>
+			<div className="w-1/4 h-full flex flex-col items-center justify-end">
+				<div className="text-white mb-0.5">{resultsEntries[2].name}</div>
+				<div className="text-white mb-1">{resultsEntries[2].score}</div>
+				<div className="w-full h-1/3 bg-gray-500 flex items-start justify-center p-8">
+					<Image alt="medal 3" src={medal3} width={100} />
+				</div>
+			</div>
+		</main>
 	);
 }
