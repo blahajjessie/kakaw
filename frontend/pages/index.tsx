637698b81f1563028759bb90851fc5f9b86488b5
Ben Grant <benjamin.j.grant@gmail.com>
2023-05-08 15:52:23 -0700

    Make join button on home functional

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,27 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 import logo from 'public/logo.png';
 import Link from 'next/link';
 
 export default function Home() {
+	const [gameId, setGameId] = useState('');
+	const [username, setUsername] = useState('');
+	const router = useRouter();
+
+	function isGameJoinable(): boolean {
+		return /[0-9]{6}/g.test(gameId) && username.length > 0;
+	}
+
+	function joinGame() {
+		if (!isGameJoinable()) {
+			return;
+		}
+
+		router.push(`/play/${gameId}?name=${encodeURIComponent(username)}`);
+	}
+
 	return (
 		<main className="bg-purple-100 flex min-h-screen flex-col items-center justify-center">
 			<div className="flex w-full max-w-sm flex-col items-center justify-center font-extrabold">
@@ -25,6 +43,8 @@ export default function Home() {
 						placeholder="Code"
 						maxLength={8}
 						required
+						value={gameId}
+						onChange={(e) => setGameId(e.target.value)}
 					/>
 					<input
 						className="bg-gray-100 border-1 border-gray-200 rounded-xl w-full px-4 py-2 mb-4 text-center text-lg shadow-md"
@@ -33,10 +53,14 @@ export default function Home() {
 						placeholder="Username"
 						maxLength={30}
 						required
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
 					/>
 					<button
 						className="bg-orange-200 hover:brightness-110 border-1 border-gray-200 rounded-xl w-full px-4 py-2 text-white text-center text-lg shadow-md"
 						type="button"
+						disabled={!isGameJoinable()}
+						onClick={joinGame}
 					>
 						Join
 					</button>
