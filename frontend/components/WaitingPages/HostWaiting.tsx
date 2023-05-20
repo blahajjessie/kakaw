59023f788370896159ba66f2c09d71b9215e2cf1
celdond <celdond@gmail.com>
2023-05-19 18:36:40 -0700

    adjustments to layout and color

--- frontend/components/WaitingPages/HostWaiting.tsx
+++ frontend/components/WaitingPages/HostWaiting.tsx
@@ -24,10 +24,12 @@ export default function HostWaiting({ gameId }: hostProps) {
 	const playerList = useContext(playerListContext) as playerListContextType;
 
 	// Alters State of timeLimit for every change
-	function handleTimeChange(e: ChangeEvent<HTMLInputElement>) {}
+	function increment(e: ChangeEvent<HTMLInputElement>) {}
 
 	// Alters State of maxPlayers for every change
-	function handlePlayerChange(e: ChangeEvent<HTMLInputElement>) {}
+	function decrement(e: ChangeEvent<HTMLInputElement>) {
+		
+	}
 
 	// Sends the Server a call to start the game
 	// NOTE: The server does send json to respond
@@ -54,7 +56,7 @@ export default function HostWaiting({ gameId }: hostProps) {
 
 	return (
 		<main
-			className={`${inter.className} w-full font-sans bg-purple-100 flex flex-col p-6`}
+			className={`${inter.className} w-screen h-screen font-sans bg-purple-100 flex flex-col p-6`}
 		>
 			<div className="flex flex-row items-center justify-start">
 				<Image
@@ -69,7 +71,7 @@ export default function HostWaiting({ gameId }: hostProps) {
 				/>
 				<div className="bg-gray-100 flex flex-col sm:flex-row items-center justify-center sm:justify-between font-extrabold shadow-heavy rounded-xl p-6 w-full sm:w-min">
 					<div>{qrCode}</div>
-					<div className="flex flex-col items-center justify-center">
+					<div className="flex flex-col items-center justify-center mx-2">
 						<div className="text-4xl xl:text-5xl whitespace-nowrap">
 							Join with the code:
 						</div>
@@ -78,13 +80,13 @@ export default function HostWaiting({ gameId }: hostProps) {
 							at https://www.kakaw.com
 						</div>
 					</div>
-					<div className="flex flex-col p-4">
+					<div className="bg-white flex flex-col rounded-xl p-4">
 						<div className="text-4xl p-2 xl:text-5xl">Settings:</div>
 						<div className="flex flex-row">
 							<div className="flex flex-row p-1">
 								<div className="flex flex-col items-center justify-center text-xl xl:text-2xl whitespace-nowrap">
-									<div className="mb-2">Time Limit:</div>
-									<div className="mb-2">Max Players:</div>
+									<div className="mb-4">Time Limit:</div>
+									<div className="mb-4">Max Players:</div>
 								</div>
 								<div className="flex flex-col items-center justify-center text-xl xl:text-2xl whitespace-nowrap">
 									<div className="flex flex-row justify-between items-center rounded-xl border-2 border-gray-200 mb-2 mx-2 w-28 h-11">
@@ -111,32 +113,45 @@ export default function HostWaiting({ gameId }: hostProps) {
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col items-start justify-start w-full p-10 shadow-heavy rounded-xl">
-				<div className="text-4xl font-extrabold py-4">Participants</div>
+			<div className="flex flex-col items-start justify-start w-full h-full shadow-heavy rounded-xl bg-gray-100 bg-opacity-50">
+				<div className="rounded-xl bg-purple-50 text-4xl font-extrabold py-4">
+					Participants
+				</div>
 				<table className="w-full text-2xl font-extrabold">
 					{displayPlayers.map((subArray, index) => (
 						<tr key={index}>
 							{subArray.map((player, i) => (
-								<td className="py-4" key={index + i}>
+								<td className="rounded-xl bg-purple-50" key={index + i}>
 									{player}
 								</td>
 							))}
 						</tr>
 					))}
 				</table>
-				<div>
+				<div className="rounded-xl bg-purple-100 text-white">
 					{playerList.length}/{maxPlayers}
 				</div>
 			</div>
-			<button
-				className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl xl:text-5xl shadow-heavy w-full h-5"
-				type="button"
-				onClick={() => {
-					startQuiz;
-				}}
-			>
-				Start
-			</button>
+			<div>
+				<button
+					className="bg-purple-50 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl xl:text-5xl shadow-heavy w-1/4 h-24"
+					type="button"
+					onClick={() => {
+						startQuiz;
+					}}
+				>
+					Back
+				</button>
+				<button
+					className="bg-orange-200 hover:bg-orange-100 border-1 border-gray-200 rounded-xl mx-2 text-white text-center text-4xl xl:text-5xl shadow-heavy w-2/3 h-24"
+					type="button"
+					onClick={() => {
+						startQuiz;
+					}}
+				>
+					Start
+				</button>
+			</div>
 		</main>
 	);
 }
