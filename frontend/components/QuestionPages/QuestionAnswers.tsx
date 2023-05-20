c171ae98617a71f8dad09f4fb5660e044632b869
Shea-Scarlet <gea.loro@gmail.com>
2023-05-19 21:24:12 -0700

    Modified Files

--- frontend/components/QuestionPages/QuestionAnswers.tsx
+++ frontend/components/QuestionPages/QuestionAnswers.tsx
@@ -1,48 +1,115 @@
+import { useState } from 'react';
 import MatchMediaWrapper from '@/components/MatchMediaWrapper';
+import Image from 'next/image';
+import X from 'public/remove 1.png';
+import Y from 'public/check-mark 1.png';
+import YourAnswer from 'public/YouAnswered.png';
 
-interface QuestionAnswerProps {
-	answers: [string, string, string, string];
+import styles from '@/styles/flip.module.css';
+
+interface QuestionAnswersProps {
+  answers: [string, string, string, string];
+  onAnswerClick: (answerIndex: number) => void;
+  selectedAnswerIndex: number | null;
 }
 
-export default function QuestionAnswers({ answers }: QuestionAnswerProps) {
-	const mobileContent = (
-		<div className="w-11/12 h-2/3 flex flex-col justify-between font-extrabold text-lg my-2">
-			<div className="w-full h-1/4 bg-red-400 grid items-center justify-center text-center rounded-xl p-4 my-2 overflow-y-auto shadow-heavy hover:brightness-110">
-				{answers[0]}
-			</div>
-			<div className="w-full h-1/4 bg-green-400 grid items-center justify-center text-center rounded-xl p-4 my-2 overflow-y-auto shadow-heavy hover:brightness-110">
-				{answers[1]}
-			</div>
-			<div className="w-full h-1/4 bg-blue-400 grid items-center justify-center text-center rounded-xl p-4 my-2 overflow-y-auto shadow-heavy hover:brightness-110">
-				{answers[2]}
-			</div>
-			<div className="w-full h-1/4 bg-yellow-400 grid items-center justify-center text-center rounded-xl p-4 my-2 overflow-y-auto shadow-heavy hover:brightness-110">
-				{answers[3]}
-			</div>
-		</div>
-	);
+export default function QuestionAnswers({
+  answers,
+  onAnswerClick,
+  selectedAnswerIndex,
+}: QuestionAnswersProps) {
+  // State to keep track of selected answers and selected image
+  const [selectedAnswers, setSelectedAnswers] = useState<boolean[]>([false, false, false, false]);
+  const [selectedImage, setSelectedImage] = useState(X); // Default image is X
 
-	const desktopContent = (
-		<div className="flex flex-wrap items-center justify-center grow gap-x-16 w-full font-extrabold text-3xl my-2 2xl:text-4xl">
-			<div className="bg-red-400 grid items-center justify-center rounded-xl w-2/5 h-2/5 px-16 py-8 my-2 text-center overflow-y-auto shadow-heavy cursor-pointer hover:brightness-110">
-				{answers[0]}
-			</div>
-			<div className="bg-green-400 grid items-center justify-center rounded-xl w-2/5 h-2/5 px-16 py-8 my-2 text-center overflow-y-auto shadow-heavy cursor-pointer hover:brightness-110">
-				{answers[1]}
-			</div>
-			<div className="bg-blue-400 grid items-center justify-center rounded-xl w-2/5 h-2/5 px-16 py-8 my-2 text-center overflow-y-auto shadow-heavy cursor-pointer hover:brightness-110">
-				{answers[2]}
-			</div>
-			<div className="bg-yellow-400 grid items-center justify-center rounded-xl w-2/5 h-2/5 px-16 py-8 my-2 text-center overflow-y-auto shadow-heavy cursor-pointer hover:brightness-110">
-				{answers[3]}
-			</div>
-		</div>
-	);
+  const handleAnswerClick = (index: number) => {
+    // Call the provided onAnswerClick function
+    onAnswerClick(index);
+    // Update all selected answers to true
+    const updatedSelectedAnswers = Array.from({ length: answers.length }, () => true);
+    setSelectedAnswers(updatedSelectedAnswers);
+    // Choose image X or Y based on the index
+    setSelectedImage(index === 0 ? X : Y);
+  };
 
-	return (
-		<MatchMediaWrapper
-			mobileContent={mobileContent}
-			desktopContent={desktopContent}
-		/>
-	);
+  const mobileContent = (
+    <div className="w-11/12 h-2/3 flex flex-col justify-between font-extrabold text-lg my-2">
+      {answers.map((answer, index) => (
+        <div
+          key={index}
+          className={`relative w-full h-1/4 ${
+            index === 0
+              ? 'bg-red-400'
+              : index === 1
+              ? 'bg-green-400'
+              : index === 2
+              ? 'bg-blue-400'
+              : 'bg-yellow-400'
+          } grid items-center justify-center text-center rounded-xl p-4 my-2 overflow-y-auto shadow-heavy hover:brightness-110`}
+          onClick={() => handleAnswerClick(index)}
+        >
+          {/* Display Your Answer Image if the current index matches selectedAnswerIndex */}
+          {index === selectedAnswerIndex && (
+            <div className="absolute bottom-0 left-0 w-full flex justify-center items-end">
+              <Image src={YourAnswer} alt="Your Answer Image" className="w-full h-full" />
+            </div>
+          )}
+
+          {/* Flip Animation */}
+          <div className={`${styles['answer-container']} ${selectedAnswers[index] ? styles.flip : ''}`}>
+            <div className={styles.answer}>{answer}</div>
+          </div>
+
+          {/* Display X or Y image if the current index matches selectedAnswerIndex */}
+          {selectedAnswers[index] && (
+            <div className="absolute top-0 right-0 h-16 w-16">
+              <Image src={selectedImage} alt="Selected Image" className="w-full h-full" />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+
+  const desktopContent = (
+    <div className="flex flex-wrap items-center justify-center grow gap-x-16 w-full font-extrabold text-3xl my-2 2xl:text-4xl">
+      {answers.map((answer, index) => (
+        <div
+          key={index}
+          className={`relative ${
+            index === 0
+              ? 'bg-red-400'
+              : index === 1
+              ? 'bg-green-400'
+              : index === 2
+              ? 'bg-blue-400'
+              : 'bg-yellow-400'
+          } grid items-center justify-center rounded-xl w-2/5 h-2/5 px-16 py-8 my-2 text-center overflow-y-auto shadow-heavy cursor-pointer hover:brightness-110`}
+          onClick={() => handleAnswerClick(index)}
+        >
+          {/* Display Your Answer Image if the current index matches selectedAnswerIndex */}
+          {index === selectedAnswerIndex && (
+            <div className="absolute bottom-0 left-0 w-full flex justify-center items-end">
+              <Image src={YourAnswer} alt="Your Answer Image" className="w-full h-full" />
+            </div>
+          )}
+
+          {/* Flip Animation */}
+          <div className={`${styles['answer-container']} ${selectedAnswers[index] ? styles.flip : ''}`}>
+            <div className={styles.answer}>{answer}</div>
+          </div>
+
+          {/* Display X or Y image if the current index matches selectedAnswerIndex */}
+          {selectedAnswers[index] && (
+            <div className="absolute top-0 right-0 h-16 w-16">
+              <Image src={selectedImage} alt="Selected Image" className="w-full h-full" />
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+
+  // Render the appropriate content based on the screen size
+  return <MatchMediaWrapper mobileContent={mobileContent} desktopContent={desktopContent} />;
 }
--- frontend/pages/[gameId]/questions/player.tsx
+++ frontend/pages/[gameId]/questions/player.tsx
@@ -1,27 +1,86 @@
+import { useState } from 'react';
 import QuestionTop from '@/components/QuestionPages/QuestionTop';
 import QuestionAnswers from '@/components/QuestionPages/QuestionAnswers';
 import PlayerQuestionBottom from '@/components/QuestionPages/PlayerQuestionBottom';
 
+import Image from 'next/image';
+import GoodJob from 'public/GoodJob.png';
+import NoLuck from 'public/NoLuck.png';
+
 export default function PlayerQuestionPage() {
-	return (
-		<main className="bg-purple-100 flex flex-col h-screen items-center">
-			<QuestionTop
-				qNum={1}
-				qText={'What is your quest?'}
-				qTime={75}
-			></QuestionTop>
-			<QuestionAnswers
-				answers={[
-					'To pass 115a',
-					'To make a real app',
-					'To have something to put on my GitHub',
-					'To seek the holy grail',
-				]}
-			></QuestionAnswers>
-			<PlayerQuestionBottom
-				name={'Student Name'}
-				score={5000}
-			></PlayerQuestionBottom>
-		</main>
-	);
+  // State variables
+  const [showModal, setShowModal] = useState(false); // Modal visibility state
+  const [selectedAnswer, setSelectedAnswer] = useState<number | null>(null); // Selected answer state
+  const [selectedImage, setSelectedImage] = useState(GoodJob); // Default image is GoodJob
+
+  // Toggle modal visibility
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  // Handle answer click event
+  const handleAnswerClick = (answerIndex: number) => {
+    setSelectedAnswer(answerIndex);
+
+    // Determine the selected image path based on your conditions
+    const imagePath = determineSelectedImagePath(answerIndex);
+    setSelectedImage(imagePath);
+
+    toggleModal();
+  };
+
+  // Function to determine the selected image path based on conditions
+  const determineSelectedImagePath = (answerIndex: number) => {
+    // Add your conditions here to choose between GoodJob and NoLuck images
+    // For example:
+    if (answerIndex === 0) {
+      return NoLuck; // Select NoLuck image
+    } else {
+      return GoodJob; // Select GoodJob image
+    }
+  };
+
+  return (
+    <main className="bg-purple-100 flex flex-col h-screen items-center">
+      {/* Render the question header */}
+      <QuestionTop qNum={1} qText={'What is your quest?'} qTime={75}></QuestionTop>
+
+      {/* Render the question answers */}
+      <QuestionAnswers
+        answers={[
+          'To pass 115a',
+          'To make a real app',
+          'To have something to put on my GitHub',
+          'To seek the holy grail',
+        ]}
+        selectedAnswerIndex={selectedAnswer}
+        onAnswerClick={handleAnswerClick}
+      ></QuestionAnswers>
+
+      {/* Render the player question bottom */}
+      <PlayerQuestionBottom name={'Student Name'} score={5100}></PlayerQuestionBottom>
+
+      {/* Render the modal when showModal is true */}
+      {showModal && (
+        <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50">
+          {/* Render the selected image if selectedAnswer is not null */}
+          {selectedAnswer !== null && (
+            <Image
+              src={selectedImage}
+              alt="Popup Image"
+              className="max-w-full max-h-full"
+            />
+          )}
+
+          {/* Close button */}
+          <button
+            className="absolute top-4 right-4 text-white text-xl"
+            onClick={toggleModal}
+          >
+            Close
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
