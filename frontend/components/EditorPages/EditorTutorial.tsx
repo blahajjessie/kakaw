6c816faef95630dd595d12cb9c294f0830e11702
Jasmine Tai <jasminecktai@gmail.com>
2023-05-21 22:55:46 -0700

    Finish editor start page

--- frontend/components/EditorPages/EditorTutorial.tsx
+++ frontend/components/EditorPages/EditorTutorial.tsx
@@ -22,7 +22,7 @@ export default function EditorTutorial({ tutorialText }: EditorTutorialProps) {
 		<div className="absolute w-full h-full pointer-events-none">
 			<div className="w-1/4 h-2/5 absolute bottom-[68%] right-[8%] pointer-events-auto">
 				{showTapHelp && (
-					<div className="absolute bottom-[35%] right-[55%] w-[40%] h-[40%] text-sm p-2 z-30 lg:text-base 2xl:text-xl">
+					<div className="absolute bottom-[30%] right-[55%] w-[40%] h-[40%] text-sm p-2 z-30 lg:text-base 2xl:text-xl">
 						<Image
 							alt="small speech bubble"
 							src={speech_small}
@@ -60,19 +60,14 @@ export default function EditorTutorial({ tutorialText }: EditorTutorialProps) {
 
 			{showTutorial && (
 				<div className="absolute w-full h-full">
-					<Image
-						alt="big speech bubble"
-						src={speech_big}
-						width={200}
-						style={{
-							position: 'absolute',
-							top: '22%',
-							left: '34%',
-							width: '43%',
-							pointerEvents: 'auto',
-						}}
-						onClick={() => setShowTutorial(false)}
-					/>
+					<div className="absolute top-[22%] left-[34%] w-[43%] h-1/2 pointer-events-auto">
+						<Image
+							alt="big speech bubble"
+							src={speech_big}
+							fill
+							onClick={() => setShowTutorial(false)}
+						/>
+					</div>
 					<div
 						className="absolute top-0 bottom-0 left-0 right-0 m-auto w-1/4 h-1/3 flex items-center justify-center text-center whitespace-pre-line z-20 pointer-events-auto text-sm sm:text-base lg:text-lg 2xl:text-2xl"
 						onClick={() => setShowTutorial(false)}
--- frontend/components/EditorPages/TimerSetter.tsx
+++ frontend/components/EditorPages/TimerSetter.tsx
@@ -1,23 +1,87 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+const DEFAULT_TIMER_VALUE = 15;
+const MIN_TIMER_VALUE = 1;
+const MAX_TIMER_VALUE = 420;
 
 export default function TimerSetter() {
-	const [timerValue, setTimerValue] = useState(15);
+	const [timerValue, setTimerValue] = useState(DEFAULT_TIMER_VALUE);
+	const [isIncrementing, setIsIncrementing] = useState(false);
+	const [isDecrementing, setIsDecrementing] = useState(false);
 
-	function updateTimerValue(newValue: number) {
-		if (newValue < 1) {
-			setTimerValue(1);
-		} else if (newValue > 420) {
-			setTimerValue(420);
+	// Store references to timeout and interval for holding -/+ buttons
+	let delayIntervalRef = useRef<NodeJS.Timeout>();
+	let intervalRef = useRef<NodeJS.Timer>();
+
+	// Incrementing/decrementing based on mouse down and mouse up events
+	useEffect(() => {
+		if (isIncrementing) {
+			// Click + button: increment by 1
+			setTimerValue((current) => current + 1);
+			// Hold + button: keep incrementing
+			delayIntervalRef.current = setTimeout(() => {
+				intervalRef.current = setInterval(() => {
+					setTimerValue((current) => current + 1);
+				}, 100);
+			}, 500);
+		} else if (isDecrementing) {
+			// Click - button: decrement by 1
+			setTimerValue((current) => current - 1);
+			// Hold - button: keep decrementing
+			delayIntervalRef.current = setTimeout(() => {
+				intervalRef.current = setInterval(() => {
+					setTimerValue((current) => current - 1);
+				}, 100);
+			}, 500);
 		} else {
-			setTimerValue(newValue);
+			// Clear timeout and interval if both isIncrementing and
+			// isDecrementing are set to false
+			clearTimeout(delayIntervalRef.current);
+			clearInterval(intervalRef.current);
+			delayIntervalRef.current = undefined;
+			intervalRef.current = undefined;
+		}
+	}, [isIncrementing, isDecrementing]);
+
+	// Stop if the minimum or maximum timer value is reached
+	useEffect(() => {
+		if (
+			(timerValue === MIN_TIMER_VALUE && isDecrementing) ||
+			(timerValue === MAX_TIMER_VALUE && isIncrementing)
+		) {
+			clearTimeout(delayIntervalRef.current);
+			clearInterval(intervalRef.current);
+			delayIntervalRef.current = undefined;
+			intervalRef.current = undefined;
+		}
+	}, [timerValue, isIncrementing, isDecrementing]);
+
+	function startIncrementing() {
+		if (timerValue < MAX_TIMER_VALUE) {
+			setIsIncrementing(true);
 		}
 	}
 
+	function stopIncrementing() {
+		setIsIncrementing(false);
+	}
+
+	function startDecrementing() {
+		if (timerValue > MIN_TIMER_VALUE) {
+			setIsDecrementing(true);
+		}
+	}
+
+	function stopDecrementing() {
+		setIsDecrementing(false);
+	}
+
 	return (
-		<div className="w-16 bg-gray-100 flex flex-row items-center justify-between font-normal text-sm p-0.5 rounded-lg xl:text-base 2xl:text-xl">
+		<div className="w-16 h-full bg-gray-100 flex flex-row items-center justify-between font-normal text-base rounded-lg lg:text-lg lg:w-20 2xl:text-xl 2xl:w-24">
 			<button
-				className="w-5 h-5 bg-white flex items-center justify-center rounded-md"
-				onClick={() => updateTimerValue(timerValue - 1)}
+				className="w-4 h-4 bg-white flex items-center justify-center text-red-200 rounded-md ml-1 my-1 lg:w-5 lg:h-5 2xl:w-7 2xl:h-7"
+				onMouseDown={startDecrementing}
+				onMouseUp={stopDecrementing}
 			>
 				-
 			</button>
@@ -25,8 +89,9 @@ export default function TimerSetter() {
 			<div>{timerValue}</div>
 
 			<button
-				className="w-5 h-5 bg-white flex items-center justify-center rounded-md"
-				onClick={() => updateTimerValue(timerValue + 1)}
+				className="w-4 h-4 bg-white flex items-center justify-center text-green-200 rounded-md mr-1 my-1 lg:w-5 lg:h-5 2xl:w-7 2xl:h-7"
+				onMouseDown={startIncrementing}
+				onMouseUp={stopIncrementing}
 			>
 				+
 			</button>
