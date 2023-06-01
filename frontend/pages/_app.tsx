153a7d94f984dfbe1ba2170327d67cf797a70e9d
Jasmine Tai <jasminecktai@gmail.com>
2023-05-31 17:34:36 -0700

    Favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,6 +10,26 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
 			<main className={inter.className}>
+				<Head>
+					<link rel="shortcut icon" href="/favicon/favicon.ico" />
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/favicon/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon/favicon-16x16.png"
+					/>
+				</Head>
 				<Component {...pageProps} />
 			</main>
 		</RecoilRoot>
