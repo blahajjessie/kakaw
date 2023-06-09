e9003f5c090637d33bc55ebb54917c06857b5b64
Ben Grant <benjamin.j.grant@gmail.com>
2023-06-09 02:32:49 -0700

    Update URLs

--- backend/src/preferences.ts
+++ backend/src/preferences.ts
@@ -1,7 +1,7 @@
 export const prefs = {
-	hostDisconnectDelay: 10000,
+	hostDisconnectDelay: 5 * 60 * 1000,
 	corsOff: true,
-	frontEndUrl: 'http://localhost:3000',
+	frontEndUrl: 'https://kakaw.190n.dev',
 	backendPort: 8080,
 	debug: false,
 	actionUpdateFrequency: 1000,
